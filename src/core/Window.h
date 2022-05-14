9c55349d787d7e0b030713ccb4183c70076acaac
zemiguel20 <jmmscoutinho@gmail.com>
2022-05-14 16:38:35 +0200

    Window.h - added comments

--- src/core/Window.h
+++ src/core/Window.h
@@ -2,13 +2,22 @@
 
 #include <SDL.h>
 
+/* @brief Static window class.
+* Manage static window instance.
+*/
 class Window
 {
-	/* Static window instance and interface */
 private:
 	static Window* instance;
 public:
+	/* @brief Initializes static window instance.
+	* Should be called once at the start.
+	*/
 	static void Init(const char* name, int width, int height);
+
+	/* @brief Destroys window and cleanup.
+	* Should be called at the end.
+	*/
 	static void Destroy();
 
 	static SDL_Window* GetNativeWindow();
