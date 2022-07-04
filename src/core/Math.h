fd2856562fe2162834c9b7ac493c466a240a4929
zemiguel20 <jmmscoutinho@gmail.com>
2022-07-05 00:06:44 +0200

    Added operators to vec2

--- src/core/Math.h
+++ src/core/Math.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <iostream>
+
 struct vec2
 {
 	float x, y;
 
 	vec2() : x(0.0f), y(0.0f) {}
 	vec2(float x, float y) : x(x), y(y) {}
+
+	vec2 operator+(const vec2& other) { return vec2(x + other.x, y + other.y); }
+	vec2& operator+=(const vec2& other) { x += other.x; y += other.y; return *this; }
+
+	vec2 operator-(const vec2& other) { return vec2(x - other.x, y - other.y); }
+	vec2& operator-=(const vec2& other) { x -= other.x; y -= other.y; return *this; }
+
+	vec2 operator*(float scalar) { return vec2(x * scalar, y * scalar); }
+	vec2& operator*=(float scalar) { x *= scalar; y *= scalar; return *this; }
+
+	friend std::ostream& operator<<(std::ostream& os, const vec2& vec) { return os << "(" << vec.x << "," << vec.y << ")"; };
 };
