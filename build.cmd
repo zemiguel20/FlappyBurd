e32aac58c84644246bec21d78a570422999f5ce2
zemiguel20 <jmmscoutinho@gmail.com>
2022-08-30 02:52:07 +0200

    fixed build script with new folder names

--- build.cmd
+++ build.cmd
@@ -1,10 +1,14 @@
 @echo off
 set BUILD_FOLDER=.\build\windows
-set RAYLIB_FOLDER=.\3rd\raylib
+set RAYLIB_FOLDER=.\dependencies\raylib
 
 if not exist %BUILD_FOLDER%\ mkdir %BUILD_FOLDER%
 
-clang++ "src\*.cpp" -Wall -o %BUILD_FOLDER%\FlappyBurd.exe -std=c++17 -I %RAYLIB_FOLDER%\include -L %RAYLIB_FOLDER%\lib -l raylibdll.lib
+clang++ source\*.cpp -Wall -o %BUILD_FOLDER%\FlappyBurd.exe ^
+-std=c++17 ^
+-O3 ^
+-I %RAYLIB_FOLDER%\include ^
+-L %RAYLIB_FOLDER%\lib -l raylibdll.lib
 
-Xcopy .\res %BUILD_FOLDER%\res\ /e /y
+Xcopy .\assets %BUILD_FOLDER%\assets\ /e /y
 Xcopy %RAYLIB_FOLDER%\lib\raylib.dll %BUILD_FOLDER%\ /e /y
