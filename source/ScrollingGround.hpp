37ffccc5c05ce4c73ddcb2cc422bf9ca1b84319c
zemiguel20 <jmmscoutinho@gmail.com>
2022-09-10 13:26:15 +0200

    Reviewed scrolling ground

--- source/ScrollingGround.hpp
+++ source/ScrollingGround.hpp
@@ -2,45 +2,48 @@
 
 #include "Core/Core.h"
 using namespace Core;
-#include "Config.h"
 #include <vector>
 
-// TODO: REVIEW IMPLEMENTATION
 class ScrollingGround
 {
 private:
-    // Texture *tex;
+    const float VELOCITY = -70.0f;
+    const float SPACING = 84.0f;
+    const Vector2 START_POS = Vector2(-150.0f, -280.0f);
+    const float X_LIMIT = -270.0f;
+
     std::vector<Transform2D> blocksTf;
+
+    Sprite *sprite;
 
 public:
     ScrollingGround()
     {
-        // tex = new Texture("assets/dirtsprite.png");
+
+        Log::Info("Loading scrolling ground");
 
         Transform2D base;
         base.scale = 1.5f;
-        // Set base at bottom-left corner
-        // base.position.x =
-        //     -(Config::REF_SCREEN_W / 2) +
-        //     tex->Width() / 2 * base.scale;
-        // base.position.y =
-        //     -(Config::REF_SCREEN_H / 2) +
-        //     tex->Height() / 2 * base.scale;
+        base.position = START_POS;
 
         blocksTf = {base, base, base, base, base, base};
 
         // Set one after another
-        // for (int i = 0; i < blocksTf.size(); i++)
-        //     blocksTf[i].position.x +=
-        //         i * tex->Width() * base.scale;
+        for (int i = 0; i < blocksTf.size(); i++)
+            blocksTf[i].position.x += i * SPACING;
+
+        sprite = new Sprite("assets/dirtsprite.png");
 
         Log::Info("Scrolling Ground loaded");
     };
 
     ~ScrollingGround()
     {
+        Log::Info("Unloading scrolling ground");
+
         blocksTf.clear();
-        // delete tex;
+        delete sprite;
+
         Log::Info("Scrolling Ground unloaded");
     };
 
@@ -49,26 +52,23 @@ public:
         // Move ground
         for (Transform2D &tf : blocksTf)
         {
-            tf.position.x -= Config::SCROLL_VEL *
-                             Time::DeltaTime();
+            tf.position.x += VELOCITY * Time::DeltaTime();
         }
         // Check to move first block to the back
-        bool firstNotVisible =
-            blocksTf[0].position.x <
-            -(Config::REF_SCREEN_W / 2) - 100.0f;
-        if (firstNotVisible)
+        if (blocksTf[0].position.x < X_LIMIT)
         {
             Transform2D block = blocksTf[0];
-            // block.position.x = blocksTf.back().position.x +
-            //                    tex->Width() * block.scale;
+            block.position.x = blocksTf.back().position.x +
+                               SPACING;
             blocksTf.erase(blocksTf.begin());
             blocksTf.push_back(block);
         }
     };
 
-    void Render(const Camera2D &cam){
-        // for (Transform2D &tf : blocksTf)
-        //     RenderSprite(cam, tf, *tex);
+    void Render(const Camera2D &cam)
+    {
+        for (Transform2D &tf : blocksTf)
+            sprite->Render(tf, cam);
     };
 
     bool CheckCollision(const Bird &player)
